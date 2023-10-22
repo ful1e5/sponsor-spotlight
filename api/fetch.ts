4bf81f8cb7de066d45984bad5add9bdea4829080
Abdulkaiz Khatri <24286590+ful1e5@users.noreply.github.com>
2023-10-22 19:16:49 +0530

    feat: fetch public sponsor data conditionally with param

--- api/fetch.ts
+++ api/fetch.ts
@@ -1,19 +1,50 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 
 import { SponsorsAPI } from "../github/SponsorsAPI";
+import { Goal, User } from "../types";
 
-export default async function (_req: VercelRequest, res: VercelResponse) {
+type ResponseData = {
+  sponsors: User[];
+  goals?: Goal | null;
+  is_sponsor?: boolean;
+};
+
+export default async function (req: VercelRequest, res: VercelResponse) {
+  let login = "";
+  let getGoals = false;
+
+  if (req.query.login) {
+    login = `${req.query.login}`;
+  }
+
+  if (req.query.goals) {
+    getGoals = true;
+  }
+
   const api = new SponsorsAPI("ful1e5");
 
   const hasSponsorsListing = await api.hasSponsorsListing();
 
   if (hasSponsorsListing) {
     const sponsors = await api.getActiveSponsors();
-    const goals = await api.getSponsorshipGoal();
     const me = await api.getMe();
     sponsors.push(me);
 
-    return res.json({ sponsors, goals });
+    const data: ResponseData = { sponsors };
+
+    let IsSponsor = false;
+    if (login) {
+      const unames = sponsors.map((s) => s.login);
+      IsSponsor = unames.includes(login);
+      data.is_sponsor = IsSponsor;
+    }
+
+    if (getGoals) {
+      const goals = await api.getSponsorshipGoal();
+      data.goals = goals;
+    }
+
+    return res.json(data);
   } else {
     return res.json({
       data: [],

9df757b199d0c07848eda2e6182da4b2bab33b73
Abdulkaiz Khatri <24286590+ful1e5@users.noreply.github.com>
2023-11-06 09:20:06 +0530

    feat: count added in single sponsor fetch

--- api/fetch.ts
+++ api/fetch.ts
@@ -5,6 +5,7 @@ import { Goal, User } from "../types";
 
 type ResponseData = {
   sponsors: User[];
+  others?: number;
   goals?: Goal | null;
   is_sponsor?: boolean;
 };
@@ -12,24 +13,28 @@ type ResponseData = {
 export default async function (req: VercelRequest, res: VercelResponse) {
   let login = "";
   let getGoals = false;
+  let getSingle = false;
 
-  if (req.query.login) {
-    login = `${req.query.login}`;
-  }
-
-  if (req.query.goals) {
-    getGoals = true;
-  }
+  if (req.query.login) login = `${req.query.login}`;
+  if (req.query.goals) getGoals = true;
+  if (req.query.single) getSingle = true;
 
   const api = new SponsorsAPI("ful1e5");
 
   const hasSponsorsListing = await api.hasSponsorsListing();
 
   if (hasSponsorsListing) {
-    const sponsors = await api.getActiveSponsors();
+    let sponsors = await api.getActiveSponsors();
+    sponsors = sponsors.sort(() => Math.random() - 0.5);
     const me = await api.getMe();
 
-    const data: ResponseData = { sponsors };
+    const data: ResponseData = {
+      sponsors: getSingle ? [sponsors[0]] : sponsors,
+    };
+
+    if (getSingle) {
+      data.others = sponsors.length - 1;
+    }
 
     let IsSponsor = false;
     if (login) {
